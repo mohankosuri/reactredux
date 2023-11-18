4bed3972920046f275a60b26a4ed7d837d6f7453
mohankosuri <kosurimohankrishna@gmail.com>
2023-11-18 11:49:56 +0530

    onechange

--- src/index.js
+++ src/index.js
@@ -6,18 +6,17 @@ import reportWebVitals from './reportWebVitals';
 import Navbar from './components/Navbar';
 import { BrowserRouter } from 'react-router-dom';
  
-import {Provider} from 'react-redux'
-import { store } from './redux/store';
+ 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Provider store={store}>
+   
     <BrowserRouter>
     <App />
     
     </BrowserRouter>
-    </Provider>
+     
   </React.StrictMode>
 );
 
--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import todoreducer from './slice/Todoslice'
-
-
-export const store = configureStore({
-    reducer:{
-         todo:todoreducer,
-    }
-})
